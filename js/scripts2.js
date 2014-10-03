8a974a7c0b9a25ebf2a11b7265a55c9958f4a60b
Josh <josh.high5@gmail.com>
2014-10-03 11:46:58 -0400

    Added a blog read more slider
    I did that thing

--- js/scripts2.js
+++ js/scripts2.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('#toggle').slideToggle(250);
 	});
+	$('.read-more').click(function(e){
+		e.preventDefault();
+		$(this).prev().slideToggle(500, function(){
+			if($(this).next().html() == "Read More"){
+				$(this).next().html("Read Less");
+			}
+			else{
+				$(this).next().html("Read More");
+			}
+		});
+	});
 });
